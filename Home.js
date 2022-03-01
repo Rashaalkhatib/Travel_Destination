272ef6d2f31ef9d36106849e626311c318bba978
Eng-Rasha AL-Khatib <86823412+Rashaalkhatib@users.noreply.github.com>
2022-03-01 20:02:00 +0200

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,1 +1,20 @@
+import Header from '/Header';
+import Footer from "/Footer";
+import tours from "./tours";
+import data from './data.json';
 
+function App(){
+    
+   
+    return(
+        <>
+            <Header />
+          
+            <Tours tours={data}/>
+            <Footer />
+        </>
+        
+    )
+}
+
+export default App;
