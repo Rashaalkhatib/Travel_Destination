85d2fb6ee764701d2ef93741103d3c0d1c01dc65
Eng-Rasha AL-Khatib <86823412+Rashaalkhatib@users.noreply.github.com>
2022-03-02 20:03:08 +0200

    Update Tour.js

--- Tour.js
+++ Tour.js
@@ -1,1 +1,24 @@
+const tour= (props) =>{
+    return(
+        <>
+        {
+            props.tour.map(tour => {
+                return(
+          <div>
+                        <img src={tour.image} alt="" />
+                        <h3>{tour.name}</h3>
+                       
+                    </div>
+                       
+                  
+                )
+                
+            })
+        }
+        </>
+        
+        
+    )
+};
 
+export default tours;
