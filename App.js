e7848b2c87c78578b5c663fac0d291ea36a21e66
Eng-Rasha AL-Khatib <86823412+Rashaalkhatib@users.noreply.github.com>
2022-03-02 19:46:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 
+import { Routes, Route } from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/city/:id" element={<tour />} />
+     
+    </Routes>
+  );
+}
+
+export default App;
